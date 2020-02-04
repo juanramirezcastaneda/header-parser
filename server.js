c6b6410e215a142817294570dc9d98a54102c4dd
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-02-04 20:33:39 +0100

    Add parameters to payload

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.get("/", function (req, res) {
 
 app.get("/api/whoami", function (req, res) {
     const ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;;
+    const language = req.headers["accept-language"];
     const userAgent = req.get('User-Agent');
     console.log(ipAddress);
     console.log(userAgent);
-    res.json({ ipaddress: ipAddress, language: '', software: 'Who am i boy' });
+    console.log(language);
+    res.json({ ipaddress: ipAddress, language: language, software: userAgent });
 });
 
 app.listen(port, () => {
